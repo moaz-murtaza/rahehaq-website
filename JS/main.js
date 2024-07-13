6c22a663c546f491d787df0bf8c3c35b69ae3c07
Moaz Murtaza <123573245+moaz-murtaza@users.noreply.github.com>
2024-07-13 20:04:38 +0500

    Slide updated main.js

--- JS/main.js
+++ JS/main.js
@@ -86,7 +86,20 @@
             $("#video").attr('src', $videoSrc);
         })
     });
-    
+
+    $(document).ready(function(){
+        $('a.nav-item.nav-link').on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+                    window.location.hash = hash;
+                });
+            }
+        });
+    });
     
     // Causes carousel
     $(".causes-carousel").owlCarousel({
